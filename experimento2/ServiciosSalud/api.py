a6d54d59653dd65d53992aeffe631ca59a2817ec
Clayderman Rojas <clayderman.rojas@gmail.com>
2021-10-03 17:36:11 -0700

    Se remueve HTTPS

--- experimento2/ServiciosSalud/api.py
+++ experimento2/ServiciosSalud/api.py
@@ -120,4 +120,4 @@ if __name__ == '__main__':
     db.create_all()
     _ = DatosPrueba()
     _.cargarDatosPrueba()
-    app.run(debug=True, host='0.0.0.0', ssl_context='adhoc')
+    app.run(debug=True, host='0.0.0.0')
